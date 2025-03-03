523149f058bd61da0211a379a3759f5eb5a41e33
stephany-ribeiro-dev <stephanysr@hotmail.com>
2025-03-03 15:06:57 -0300

    feat:add logic for function list-friends

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
 const listaAmigos = [];
 
-function exibirTexto(tag, texto) {
-
-    let campo = document.querySelector(tag);
-
-    campo.innerHTML = texto;
-
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', { rate: 1.2 });
-
-}
-
-function textoInicial() {
-
-    exibirTexto('h1', 'Amigo Secreto');
-    exibirTexto('h2', 'Digite o nome dos seus amigos');
-
-}
-textoInicial();
-
 function adicionarAmigo() {
 
     const amigo = document.querySelector('input').value;
@@ -28,12 +10,41 @@ function adicionarAmigo() {
     } else {
         listaAmigos.push(amigo);
         console.log(listaAmigos);
+        exibirAmigos();
         limparCampo();
     }
 }
 
+function exibirAmigos() {
+
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < listaAmigos.length; i++) {
+
+        const amigo = listaAmigos[i];
+        const itemLista = document.createElement('li');
+        itemLista.textContent = amigo;
+        lista.appendChild(itemLista);
+    }
+}
+
 function limparCampo() {
+
     amigo = document.querySelector('input');
     amigo.value = '';
 }
 
+function exibirTexto(tag, texto) {
+
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', { rate: 1.2 });
+}
+
+function textoInicial() {
+
+    exibirTexto('h1', 'Amigo Secreto');
+    exibirTexto('h2', 'Digite o nome dos seus amigos');
+}
+textoInicial();
