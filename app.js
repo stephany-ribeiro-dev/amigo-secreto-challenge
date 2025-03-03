951efc7f4796ec46d7b1d7c0c83b5918ee7735ca
stephany-ribeiro-dev <stephanysr@hotmail.com>
2025-03-02 23:49:14 -0300

    feat(lang): adiciona voice transcription  para português brasileiro

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const listaAmigos = [];
 
+function exibirTexto(tag, texto) {
+
+    let campo = document.querySelector(tag);
+
+    campo.innerHTML = texto;
+
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', { rate: 1.2 });
+
+}
+
+function textoInicial() {
+
+    exibirTexto('h1', 'Amigo Secreto');
+    exibirTexto('h2', 'Digite o nome dos seus amigos');
+
+}
+
+textoInicial();
 function adicionarAmigo() {
 
     const amigo = document.querySelector('input').value;
