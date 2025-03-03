336e02fb4df32d516809469a645e3c435bf97650
stephany-ribeiro-dev <stephanysr@hotmail.com>
2025-03-03 16:42:44 -0300

    fix: formating document spaces

--- app.js
+++ app.js
@@ -6,16 +6,13 @@ function adicionarAmigo() {
     console.log(amigo);
 
     if (amigo === "") {
-
         alert('Por Favor, insira um nome.');
         return;
     } else if (listaAmigos.includes(amigo)) {
-
         alert('Este amigo já foi adicionado.');
         limparCampo();
         return;
     } else {
-        
         listaAmigos.push(amigo);
         console.log(listaAmigos);
         exibirAmigos();
@@ -29,7 +26,6 @@ function exibirAmigos() {
     lista.innerHTML = '';
 
     for (let i = 0; i < listaAmigos.length; i++) {
-
         const amigo = listaAmigos[i];
         const itemLista = document.createElement('li');
         itemLista.textContent = amigo;
@@ -51,20 +47,17 @@ function sortearAmigo() {
 }
 
 function limparCampo() {
-
     amigo = document.querySelector('input');
     amigo.value = '';
 }
 
 function exibirTexto(tag, texto) {
-
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', { rate: 1.2 });
 }
 
 function textoInicial() {
-
     exibirTexto('h1', 'Amigo Secreto');
     exibirTexto('h2', 'Digite o nome dos seus amigos');
 }
